4bfd2cc68dd792e149fea88d592e1f50a51c9456
rjo6615 <robertjobernier@gmail.com>
2022-11-18 15:24:33 -0500

    got employee tests working

---
+++ tests/Employee.test.js
@@ -0,0 +1,49 @@
+const Employee = require("../lib/Employee");
+
+describe("Employee", () => {
+    describe("getName", () => {
+        it("should return inputed name", () => {
+            const id = 3;
+            const name = "Timmy";
+            const email = "timmy@mail.com";
+
+            const result = new Employee(id, name, email).getName();
+
+            expect(result).toEqual(name);
+        })
+    })
+    describe("getId", () => {
+        it("should return inputed Id", () => {
+            const id = 3;
+            const name = "Timmy";
+            const email = "timmy@mail.com";
+
+            const result = new Employee(id, name, email).getId();
+
+            expect(result).toEqual(id);
+        })
+    })
+    describe("getEmail", () => {
+        it("should return inputed email", () => {
+            const id = 3;
+            const name = "Timmy";
+            const email = "timmy@mail.com";
+
+            const result = new Employee(id, name, email).getEmail();
+
+            expect(result).toEqual(email);
+        })
+    })
+    describe("getRole", () => {
+        it("should return default role", () => {
+            const id = 3;
+            const name = "Timmy";
+            const email = "timmy@mail.com";
+            const role = "Employee";
+
+            const result = new Employee(id, name, email).getRole();
+
+            expect(result).toEqual(role);
+        })
+    })
+})
